ab4f95a13bfea3d9339c7367a76eefb974bbb0de
Gaurav Chopra <gauravchopracg@gmail.com>
2020-04-26 23:21:41 +0530

    Releasing V51

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, jsonify
 from app import app
-from utils2 import *
+from utils import *
 from app.forms import RedditForm
 from werkzeug.utils import secure_filename
 import os
@@ -12,12 +12,12 @@ def index():
     form = RedditForm()
     if form.validate_on_submit():
         path = form.url.data
-        predicted_flair, actual_flair = predict(path)
-#        path = [path]
-#        res, flair = process(list(path))
+        #predicted_flair, actual_flair = predict(path)
+        path = [path]
+        res, flair = process(list(path))
 #        a, b =detect_flair(url,loaded_model)
-#        predicted_flair=res[path[0]]#str(a[0])
-#        actual_flair = flair#b#predict(url)
+        predicted_flair=res[path[0]]#str(a[0])
+        actual_flair = flair#b#predict(url)
         #predicted_flair = 'Policy'#predict(url)
         #flash('Flair for URL requested is {}'.format(predicted_flair))
         #return render_template('predict.html', url=url)
@@ -29,7 +29,7 @@ def index():
 
 # automated testing endpoint
 # this endpoint will be used for testing performance of the classifier
-@app.route('/testing', methods=['GET', 'POST'])
+@app.route('/automated_testing', methods=['GET', 'POST'])
 def test():
     json = {}
     if request.method == 'POST':
@@ -45,14 +45,14 @@ def test():
 #            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 #            f = open(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'r+')
 #            urls = [line.rstrip('\n') for line in f.readlines()]
-            #res = process(urls)
-            #json = res[0]
+            res = process(urls)
+            json = res[0]
 
             #f.close()
-            for url in urls:
-                a, _ =predict(url)
+            #for url in urls:
+            #    a, _ =predict(url)
             #    predicted_flair=str(a[0])
-                json[url] = a
+            #    json[url] = a
 
             return jsonify(json)
     return "Error in file upload"
